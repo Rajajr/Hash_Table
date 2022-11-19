0526e71628c04cb84623d05a4f853193723d150e
Rajajr <rajjr011@gmail.com>
2022-11-20 04:00:08 +0700

    UC-1 Ability to find frequency of words in a sentence

---
+++ HashTables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // UC - 1 Ability to find frequency   of words in a sentence like “To be or not to be”
+            Console.WriteLine("Welcome to Hash Tables");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
+            hash.Add(1, "To");
+            hash.Add(2, "be");
+            hash.Add(3, "or");
+            hash.Add(4, "not");
+            hash.Add(5, "to");
+            hash.Add(6, "be");
+
+            for (int i = 0; i <= 6; i++)
+            {
+                Console.WriteLine(hash.Get(i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
